bc4320bfda2909cc7d4c8d32947dff509fbcbe3f
Nikita Yelizarov <henry141993@mail.ru>
2018-01-13 22:44:13 +0300

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-DB = $1
+#DB = $1
 
 
-pgdbf ADDROBJ.DBF | iconv -f cp866 -t utf-8 | psql $DB
-pgdbf SOCRBASE.DBF | iconv -f cp866 -t utf-8 | psql $DB
+#pgdbf ADDROBJ.DBF | iconv -f cp866 -t utf-8 | psql $DB
+#pgdbf SOCRBASE.DBF | iconv -f cp866 -t utf-8 | psql $DB
+
+for FILENAME in `find . -type f -name "ADDROB*.DBF"`
+do
+pgdbf $FILENAME | iconv -f cp866 -t utf-8 | psql -h 0.0.0.0 -U admin -d fias
+done
+
+pgdbf SOCRBASE.DBF | iconv -f cp866 -t utf-8 | psql -h 0.0.0.0 -U admin -d fias
